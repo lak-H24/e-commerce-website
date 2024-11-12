aa8d5c85a077b116ea48ac2256125feebbf846fe
lak-H24 <lak.242004@gmail.com>
2024-11-12 17:48:20 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {gradientColorStops:{
+      "linear1":"#376E6F",
+      "linear2":"#FEFEFE"
+
+    },backgroundColor:{
+      "bgcolor1":"#1C3334"
+    }},
+  },
+  plugins: [],
+}
+
